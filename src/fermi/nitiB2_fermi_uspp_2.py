c7ee42f28bf9ed5084765067a0e469f245ff4ad8
LutherChris <chrisluther@tutamail.com>
2026-09-16 13:52:08 +0200

    lokaler neuer Code

--- src/fermi/nitiB2_fermi_uspp_2.py
+++ src/fermi/nitiB2_fermi_uspp_2.py
@@ -4,15 +4,11 @@ from lib.run_fermi import run
 # Parameter des Plots
 # ###################################################################################
 
-#gnufile = "nitiB2_fermi_2.dat.gnu"
-gnufile = "nitiB2_point2A.dat.gnu"
-#fermi_energy = 16.5866 # Fermi-Energie für PP 1.3.0
-fermi_energy = 17.5790 # Fermi-Energie für PP 2.0
+gnufile = "nitiB2_fermi_2.dat.gnu"
+fermi_energy = 16.5866
 plot_fermilevel=True
-#x_coordinates = [0, 0.8660, 1.3660, 1.8660, 2.3660]
-#x_labels = ["$\\Gamma$", "R", "M", "X", "$\\Gamma$"]
-x_coordinates = [0, 0.8660]
-x_labels = ["$\\Gamma$", "R"]
+x_coordinates = [0, 0.8660, 1.3660, 1.8660, 2.3660]
+x_labels = ["$\\Gamma$", "R", "M", "X", "$\\Gamma$"]
 
 if __name__ == "__main__":
     run(cfg=__import__(__name__))
